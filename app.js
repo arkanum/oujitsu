29600305c35fe66f971da15c02a5b9a24faef031
paracelsus-von <phara0h1@hotmail.com>
2015-10-06 14:46:16 +0100

    Update app.js
    Fixed image align for 'What is jitsu' section.

--- app.js
+++ app.js
@@ -22,6 +22,25 @@ var main = function() {
         nextDot.addClass('active-dot')
     });
     
+    $('.arrow-next-pic').click(function() {
+        var currentSlide = $('.active-slide');
+        var nextSlide = currentSlide.next();
+        
+        var currentDot = $('.active-dot');
+        var nextDot = currentDot.next();
+        
+        if (nextSlide.length == 0) {
+            nextSlide = $('.slide').first();
+            nextDot = $('.dot').first();
+        }
+        
+        currentSlide.fadeOut(400).removeClass('active-slide');
+        nextSlide.fadeIn(400).addClass('active-slide');
+        
+        currentDot.removeClass('active-dot')
+        nextDot.addClass('active-dot')
+    });
+    
     $('.arrow-prev').click(function() {
         var currentSlide = $('.active-slide');
         var prevSlide = currentSlide.prev();
